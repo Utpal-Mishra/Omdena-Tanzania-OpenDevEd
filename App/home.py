11b307aad114f69be28d043f0a0a1a49a03f0529
Utpal Mishra <utpalmishra21official@gmail.com>
2024-07-18 10:27:51 +0100

    Added Toggle Feature to Add a New School

--- App/home.py
+++ App/home.py
@@ -1,10 +1,18 @@
 # RESOURCES
+
 # https://docs.streamlit.io/develop/api-reference
 # https://www.earthdatascience.org/tutorials/introduction-to-leaflet-animated-maps/
 # *https://python-visualization.github.io/folium/latest/user_guide/map.html
 # *https://python-visualization.github.io/folium/latest/user_guide/plugins/featuregroup_subgroup.html
 # *https://python-visualization.github.io/folium/version-v0.10.1/modules.html
 # https://openweathermap.org/current
+
+# Emoji/ Icons
+# https://www.restack.io/docs/streamlit-knowledge-streamlit-emoji-guide
+# https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+# https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Outlined&selected=Material+Symbols+Outlined:add_location_alt:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=Add&icon.size=24&icon.color=%23e8eaed
+# https://fontawesome.com/icons/map-pin?f=classic&s=solid
+# https://fontawesome.com/v4/icons/
 
 ###############################################################################################################
 
@@ -165,6 +173,8 @@ def app():
         
         data['Region'] = data['Region'].apply(lambda x: x.title())
         data['SchoolName'] = data['SchoolName'].apply(lambda x: x.title())
+        # data.to_csv('data.csv', index = True)
+        # data = pd.read_csv('data.csv')
         # st.dataframe(data)
         
         st.map(data.rename(columns = {'Latitude': 'latitude', 'Longitude': 'longitude'}), zoom = 5)
@@ -463,7 +473,7 @@ def app():
                         
                         # Add-ons
                                 
-                        st.divider()
+                        st.sidebar.divider()
                         
                         st.sidebar.header('Add Location: ')
                         name = st.sidebar.text_input("Add School Name")
@@ -473,10 +483,21 @@ def app():
                         st.sidebar.write("Coordinates: ", lat, long) # name
                         # st.write(region, council, ward, ownership, lat, long)
                         
-                        data.loc[len(data.index)] = [lat, long, region, council, ward, name, ownership]
-                        # icon = folium.features.CustomIcon('/content/drive/My Drive/Colab Notebooks/pushpin.png', icon_size=(30,30))
-                        folium.Marker([lat, long], popup = name, icon=folium.Icon(color = 'red', icon = "thumb-tack", prefix='fa')).add_to(Map)
-                        folium_static(Map)
+                        if st.sidebar.toggle("Add School Location"):
+                            
+                            newdata = data
+                            newdata.loc[len(data.index)] = [lat, long, region, council, ward, name, ownership]
+                            st.balloons()
+                            st.toast('New School Location Added!!!', icon='🎉')
+                            
+                            
+                            # data.to_csv('newdata.csv', index = True)
+                            # data = pd.read_csv('newdata.csv')
+                            # st.dataframe(newdata)
+            
+                            # icon = folium.features.CustomIcon('/content/drive/My Drive/Colab Notebooks/pushpin.png', icon_size=(30,30))
+                            folium.Marker([lat, long], popup = name, icon=folium.Icon(color = 'red', icon = "thumb-tack", prefix='fa')).add_to(Map)
+                            folium_static(Map)
                         
     ############################################################################################################                   
     
